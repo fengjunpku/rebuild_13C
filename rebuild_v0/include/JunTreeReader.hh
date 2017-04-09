0830040ed22b0787bf4b9deb8fc525f0bbd5ab81
fengjunpku <fengjun2013@pku.edu.cn>
2017-04-10 01:14:31 +0800

    reader/add t1 info

--- rebuild_v0/include/JunTreeReader.hh
+++ rebuild_v0/include/JunTreeReader.hh
@@ -21,20 +21,40 @@ public:
   Double_t l0w1e[4];
   Double_t l0b7e[4];
   Double_t l0se;
+  Double_t l0t[4];
   Int_t    l0hit;
   Int_t    l0i[4];
   Int_t    l0j[4];
   Int_t    l0wi[4];
   Int_t    l0wj[4];
+  Int_t    l0m[4];
   //r0
   Double_t r0w1e[4];
   Double_t r0b7e[4];
   Double_t r0se;
+  Double_t r0t[4];
   Int_t    r0hit;
   Int_t    r0i[4];
   Int_t    r0j[4];
   Int_t    r0wi[4];
   Int_t    r0wj[4];
+  Int_t    r0m[4];
+  //l1
+  Double_t l1w1e[4];
+  Double_t l1se;
+  Double_t l1t[4];
+  Int_t    l1hit;
+  Int_t    l1wi[4];
+  Int_t    l1wj[4];
+  Int_t    l1m[4];
+  //r1
+  Double_t r1w1e[4];
+  Double_t r1se;
+  Double_t r1t[4];
+  Int_t    r1hit;
+  Int_t    r1wi[4];
+  Int_t    r1wj[4];
+  Int_t    r1m[4];
   
   JunTreeReader(int runnum);
   virtual ~JunTreeReader();
--- rebuild_v0/src/JunTreeReader.cc
+++ rebuild_v0/src/JunTreeReader.cc
@@ -45,6 +45,8 @@ void JunTreeReader::Init()
   ptree->SetBranchAddress("l0j",   l0j);
   ptree->SetBranchAddress("l0wi",  l0wi);
   ptree->SetBranchAddress("l0wj",  l0wj);
+  ptree->SetBranchAddress("l0t",   l0t);
+  ptree->SetBranchAddress("l0m",   l0m);
   //r0
   ptree->SetBranchAddress("r0w1e", r0w1e);
   ptree->SetBranchAddress("r0b7e", r0b7e);
@@ -54,6 +56,24 @@ void JunTreeReader::Init()
   ptree->SetBranchAddress("r0j",   r0j);
   ptree->SetBranchAddress("r0wi",  r0wi);
   ptree->SetBranchAddress("r0wj",  r0wj);
+  ptree->SetBranchAddress("l0t",   r0t);
+  ptree->SetBranchAddress("l0m",   r0m);
+  //l1
+  ptree->SetBranchAddress("l1w1e", l1w1e);
+  ptree->SetBranchAddress("l1se", &l1se);
+  ptree->SetBranchAddress("l1hit",&l1hit);
+  ptree->SetBranchAddress("l1wi",  l1wi);
+  ptree->SetBranchAddress("l1wj",  l1wj);
+  ptree->SetBranchAddress("l1t",   l1t);
+  ptree->SetBranchAddress("l1m",   l1m);
+  //r1
+  ptree->SetBranchAddress("r1w1e", r1w1e);
+  ptree->SetBranchAddress("r1se", &r1se);
+  ptree->SetBranchAddress("r1hit",&r1hit);
+  ptree->SetBranchAddress("r1wi",  r1wi);
+  ptree->SetBranchAddress("r1wj",  r1wj);
+  ptree->SetBranchAddress("r1t",   r1t);
+  ptree->SetBranchAddress("r1m",   r1m);
 }
 
 Int_t JunTreeReader::GetInt(const string vname,const int p)
@@ -65,12 +85,24 @@ Int_t JunTreeReader::GetInt(const string vname,const int p)
   if(vname == "l0j")   return l0j[p];
   if(vname == "l0wi")  return l0wi[p];
   if(vname == "l0wj")  return l0wj[p];
+  if(vname == "l0m")   return l0m[p];
   //r0
   if(vname == "r0hit") return r0hit;
   if(vname == "r0i")   return r0i[p];
   if(vname == "r0j")   return r0j[p];
   if(vname == "r0wi")  return r0wi[p];
   if(vname == "r0wj")  return r0wj[p];
+  if(vname == "r0m")   return r0m[p];
+  //l1
+  if(vname == "l1hit") return l1hit;
+  if(vname == "l1wi")  return l1wi[p];
+  if(vname == "l1wj")  return l1wj[p];
+  if(vname == "l1m")   return l1m[p];
+  //r1
+  if(vname == "r1hit") return r1hit;
+  if(vname == "r1wi")  return r1wi[p];
+  if(vname == "r1wj")  return r1wj[p];
+  if(vname == "r1m")   return r1m[p];
   //last
   char err[30];
   sprintf(err,"JunTreeReader::GetInt() : %s is illegal vname !",vname.c_str());
@@ -85,10 +117,20 @@ Double_t JunTreeReader::GetDou(const string vname,const int p)
   if(vname == "l0se")  return l0se;
   if(vname == "l0w1e") return l0w1e[p];
   if(vname == "l0b7e") return l0b7e[p];
+  if(vname == "l0t")   return l0t[p];
   //r0
   if(vname == "r0se")  return r0se;
   if(vname == "r0w1e") return r0w1e[p];
   if(vname == "r0b7e") return r0b7e[p];
+  if(vname == "r0t")   return r0t[p];
+  //l1
+  if(vname == "l1se")  return l1se;
+  if(vname == "l1w1e") return l1w1e[p];
+  if(vname == "l1t")   return l1t[p];
+  //r1
+  if(vname == "r1se")  return r1se;
+  if(vname == "r1w1e") return r1w1e[p];
+  if(vname == "r1t")   return r1t[p];
   //last
   char err[30];
   sprintf(err,"JunTreeReader::GetDou() : %s is illegal vname !",vname.c_str());
